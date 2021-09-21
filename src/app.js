003cd4985c49cb7a52ef105225d392b5024f4d04
audreyjanssen <88087718+audreyjanssen@users.noreply.github.com>
2021-09-21 10:49:12 +0200

    Connected API call for daily forecast with coordinates

--- src/app.js
+++ src/app.js
@@ -23,13 +23,14 @@ function dateInput(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
- 
+
   let days = ["Wed", "Thu", "Fri", "Sat"];
-  
+
   let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+  days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
@@ -51,6 +52,12 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
 
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "893886e4043b145eed504f35245dfbcc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -78,6 +85,8 @@ function displayWeather(response) {
     .setAttribute("alt", response.data.weather[0].main);
 
   celsiusTemp = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -125,4 +134,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", unitCelsius);
 
 searchCity("Amsterdam");
-displayForecast();
